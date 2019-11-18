944a386634aaa2c70cbe298f44632eaa065cd8ba
grayson-orr <graysono@op.ac.nz>
2019-11-18 21:34:38 +1300

    Create basic web server in Go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Index")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", index)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("App listening on port 3000")
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
 }
